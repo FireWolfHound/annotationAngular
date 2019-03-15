00a6f06005e9dec24d3d0d9d9994a8392e81d647
FireWolfHound <youvalabudarham@gmail.com>
2019-03-15 11:04:07 +0100

    Récupérations des Span

--- script.js
+++ script.js
@@ -1,109 +1,128 @@
-var locationAllSpan = [
+var locationAllSpan = JSON.parse(localStorage.getItem('table')) || []
 
-];
+splitingText()
+
 
 function highlightText() {
 
 	var currPosition = getSelectionPosition();
 
-	
+
 	var isOk = true;
 	for (let i = 0; i < locationAllSpan.length; i++) {
 		if (
-				(currPosition.start > locationAllSpan[i].start && currPosition.start < locationAllSpan[i].end) 
-				|| (currPosition.end > locationAllSpan[i].start && currPosition.end < locationAllSpan[i].end )
-				|| (currPosition.end > locationAllSpan[i].end && currPosition.start < locationAllSpan[i].start )
-			) {
-				isOk = false;
-			}
+			(currPosition.start > locationAllSpan[i].start && currPosition.start < locationAllSpan[i].end) ||
+			(currPosition.end > locationAllSpan[i].start && currPosition.end < locationAllSpan[i].end) ||
+			(currPosition.end > locationAllSpan[i].end && currPosition.start < locationAllSpan[i].start)
+		) {
+			isOk = false;
 		}
+	}
 
 	if (isOk) {
 		// Ajout de l'objet dans le tableau
 		locationAllSpan.push(currPosition);
-		
+
 		//on crée la balise span
 		createSpan(currPosition)
+		sortTable();
 
 		console.log(locationAllSpan);
-		
 
+		localStorage.setItem('table', JSON.stringify(locationAllSpan))
 
-	}else{
-			console.log('Déja dans le surlignement');
+	} else {
+		console.log('Déja dans le surlignement');
 	}
 }
 
 
+function splitingText() {
+	var myText = document.getElementById("annotation").textContent;
+	endText = myText.length
 
+	var arrTextNode = []
+	var ancienSpanEnd = 0;
 
+	locationAllSpan.forEach((span) => {
+		arrTextNode.push(myText.slice(ancienSpanEnd, span.start))
+		arrTextNode.push(myText.slice(span.start, span.end))
+		ancienSpanEnd = span.end
+	});
+	
+	arrTextNode.push(myText.slice(ancienSpanEnd, endText));
 
+	console.log(arrTextNode);
+	
+	var newText = "";
 
+	for (let i = 0; i < arrTextNode.length; i++) {
 
+		if (i%2) {
+			var text = '<span style="background-color: yellow">' + arrTextNode[i] + '</span>'
+			newText += text
+		}else{
+			var span = arrTextNode[i]
+			newText += span
+		}
+	}
 
+	console.log(newText);
+	document.getElementById("annotation").innerHTML = newText
+}
 
 function createSpan(currPosition) {
-		var selection = window.getSelection().getRangeAt(0);
-		var selectedText = selection.extractContents();
-		console.log(selectedText);
-		
-    var span = document.createElement("span");
-    span.style.backgroundColor = currPosition.colorSpan;
-    span.setAttribute("PosStart", currPosition.start);
-    span.setAttribute("PosEnd", currPosition.end);
-    span.appendChild(selectedText);
-		selection.insertNode(span);
-		
+	var selection = window.getSelection().getRangeAt(0);
+	var selectedText = selection.extractContents();
+	var span = document.createElement("span");
+	span.style.backgroundColor = currPosition.colorSpan;
+	span.setAttribute("PosStart", currPosition.start);
+	span.setAttribute("PosEnd", currPosition.end);
+	span.appendChild(selectedText);
+	selection.insertNode(span);
+
 }
 
 function getSelectionPosition() {
-		var range = window.getSelection().getRangeAt(0);
-    var preSelectionRange = range.cloneRange();
-    preSelectionRange.selectNodeContents(document.getElementById("annotation"));
-		preSelectionRange.setEnd(range.startContainer, range.startOffset);		
-    var start = preSelectionRange.toString().length;
-		var end = start + range.toString().length;
-		var colorSpan = getRandomColor();
-    
-    var spanLocation = {
-        start,
-				end,
-				colorSpan
-		}
+	var range = window.getSelection().getRangeAt(0);
+	var preSelectionRange = range.cloneRange();
+	preSelectionRange.selectNodeContents(document.getElementById("annotation"));
+	preSelectionRange.setEnd(range.startContainer, range.startOffset);
+	var start = preSelectionRange.toString().length;
+	var end = start + range.toString().length;
+	var colorSpan = getRandomColor();
 
-    return spanLocation;
+	var spanLocation = {
+		start,
+		end,
+		colorSpan
+	}
+
+	return spanLocation;
 };
 
 
 
 
 
-$(document).ready(function () {
-	verifTable(locationAllSpan);
-  })
+function sortTable() {
+	locationAllSpan.sort(function (a, b) {
+		return parseFloat(a.start) - parseFloat(b.start);
+	});
+}
 
-function verifTable(table) {
-
-	if (table.length != 0 ){
-		for (let i = 0; i < table.length; i++) {
-			//createSpan(table[i])			
-		}
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
 	}
+	return color;
 }
 
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-
-  function removeMarks() {
-    $(".Mark").each(function () {
-        $(this).contents().unwrap();
-    });
+function removeMarks() {
+	$(".Mark").each(function () {
+		$(this).contents().unwrap();
+	});
 }
